043cf9bd63b9d1a3553b9848b0d80f695bd158df
Bruno Amaral Ianeczek <87914395+BrunoIaneczek@users.noreply.github.com>
2023-06-18 16:38:40 -0300

    ligacaobanco

---
+++ ligacao.php
@@ -0,0 +1,11 @@
+<?php
+
+    $dbHost = 'Localhost';
+    $dbUsername = 'root';
+    $dbpassword = '';
+    $dbName = 'academia_ads';
+
+    $conexao = new mysqli($dbHost, $dbUsername,$dbpassword, $dbName);
+
+ 
+?>
